007ed4d33fe06829c3b3c753a760a12c17a273ff
lingchuL <2938843848@qq.com>
2023-12-22 20:32:55 +0800

    新增从频率到音高的方法

--- SupBack/freq_note_handle.py
+++ SupBack/freq_note_handle.py
@@ -8,13 +8,14 @@ class FreqNoteTrans(object):
 		self.notes_index_sharp_dict = {}
 		self.notes_index_flat_dict = {}
 
-		self.standard_freq = 440
-
 		for note in notes_sharp:
 			self.notes_index_sharp_dict[note] = notes_sharp.index(note)
 
 		for note in notes_flat:
 			self.notes_index_flat_dict[note] = notes_flat.index(note)
+
+		self.standard_freq = 440
+		self.standard_note_name_index = self.notes_index_sharp_dict["A"]
 
 	@staticmethod
 	def get_note_name(in_note: str):
@@ -46,19 +47,42 @@ class FreqNoteTrans(object):
 
 		note_pitch_interval = (note_b_index - note_a_index) + height_interval * 12
 
-		print(f"FreqNoteTrans get_note_pitch_interval {note_a}与{note_b}音程为{note_pitch_interval}个半音")
+		# print(f"FreqNoteTrans get_note_pitch_interval {note_a}与{note_b}音程为{note_pitch_interval}个半音")
 
 		return note_pitch_interval
 
+	def get_index_note(self, in_index):
+		for key, value in self.notes_index_sharp_dict.items():
+			if value == in_index:
+				return key
+
+		print(f"FreqNoteTrans get_index_note 音高索引{in_index}未找到对应音名")
+		return ""
+
 	def get_near_note(self, in_freq):
 		freq_times = in_freq / self.standard_freq
-		
-		pass
+		note_interval = round(math.log(freq_times, math.pow(2, 1 / 12)))
+
+		# print(f"FreqNoteTrans get_near_note 频率{in_freq}对应音程{note_interval}个半音")
+
+		height_interval = round(note_interval / 12)
+		index_interval = note_interval % 12
+
+		height = 4 + height_interval
+		note_name_index = self.standard_note_name_index + index_interval
+		note_name_index %= 12
+		note_name = self.get_index_note(note_name_index)
+
+		note = f"{note_name}{height}"
+
+		print(f"FreqNoteTrans get_near_note 频率{in_freq}最接近的音高: {note}")
+
+		return note
 
 	def get_note_freq(self, in_note):
-		note_pitch_interval = self.get_note_pitch_interval("A4", in_note)
+		note_interval = self.get_note_pitch_interval("A4", in_note)
 
-		note_freq = self.standard_freq * math.pow(2, note_pitch_interval / 12)
+		note_freq = self.standard_freq * math.pow(2, note_interval / 12)
 
 		print(f"FreqNoteTrans get_note_freq 音高{in_note}频率: {note_freq}")
 		return note_freq
@@ -67,3 +91,5 @@ class FreqNoteTrans(object):
 if __name__ == "__main__":
 	freq_note_trans = FreqNoteTrans()
 	freq_note_trans.get_note_freq("A4")
+	freq_note_trans.get_near_note(784)
+
