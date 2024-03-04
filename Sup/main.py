08c33e55eec3df265075c3b67a4ccf1c493886fb
lingchuL <2938843848@qq.com>
2024-03-04 22:40:25 +0800

    完善总体启动过程

--- Sup/main.py
+++ Sup/main.py
@@ -1,12 +1,32 @@
 import os
-import requests
-import subprocess
-
-from tqdm import tqdm
+import time
+from subprocess import Popen
 
 
-def ensure_nvm():
+def call_cmd_wait(in_cmd):
+	try:
+		p = Popen(in_cmd, shell=True)
+		p.wait()
+		return p.returncode
+	except Exception as e:
+		print(e)
+		exit(1)
+
+
+def call_cmd_nowait(in_cmd):
+	try:
+		p = Popen(in_cmd, shell=True)
+	except Exception as e:
+		print(e)
+		exit(1)
 
 
 if __name__ == "__main__":
-	FfmpegHandler.ensure_environ_temp()
+	call_cmd_wait(r".\nvm-noinstall\nvm.exe install 20.10.0")
+	call_cmd_wait(r".\nvm-noinstall\nvm.exe use 20.10.0")
+	# call_cmd_wait(r"npm config set registry https://registry.npmmirror.com")
+	call_cmd_wait(r"npm install")
+	call_cmd_nowait(r"npm run next_start")
+	time.sleep(5)
+	call_cmd_nowait(r".\electron_out\supfront.exe")
+	call_cmd_nowait(r".\supback\supback.exe")
