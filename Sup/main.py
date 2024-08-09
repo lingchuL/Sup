5f37de6b700063d3219b9a751c86a50f2241d5ed
lingchuL <2938843848@qq.com>
2024-08-09 18:14:32 +0800

    原来不需要nextjs自己启动！Electron自带的nodejs环境可以拿来启动nextjs！

--- Sup/main.py
+++ Sup/main.py
@@ -22,21 +22,21 @@ def call_cmd_nowait(in_cmd):
 
 
 if __name__ == "__main__":
-	has_npm_installed = False
-	with open("./npm_log.txt", "r") as f:
-		content = f.read()
-		if content == "done":
-			has_npm_installed = True
+	# has_npm_installed = False
+	# with open("./npm_log.txt", "r") as f:
+	# 	content = f.read()
+	# 	if content == "done":
+	# 		has_npm_installed = True
 
-	if not has_npm_installed:
-		call_cmd_wait(r".\nvm-noinstall\nvm.exe install 20.10.0")
-		call_cmd_wait(r".\nvm-noinstall\nvm.exe use 20.10.0")
-		# call_cmd_wait(r"npm config set registry https://registry.npmmirror.com")
-		call_cmd_wait(r"npm install")
-		with open("./npm_log.txt", "w+") as f:
-			f.write("done")
-	call_cmd_nowait(r"npm run next_start")
-	time.sleep(5)
+	# if not has_npm_installed:
+	# 	call_cmd_wait(r".\nvm-noinstall\nvm.exe install 20.10.0")
+	# 	call_cmd_wait(r".\nvm-noinstall\nvm.exe use 20.10.0")
+	# 	# call_cmd_wait(r"npm config set registry https://registry.npmmirror.com")
+	# 	call_cmd_wait(r"npm install")
+	# 	with open("./npm_log.txt", "w+") as f:
+	# 		f.write("done")
+	# call_cmd_nowait(r"npm run next_start")
+	# time.sleep(5)
 	call_cmd_nowait(r".\electron_out\supfront.exe")
 	call_cmd_nowait(r".\supback\supback.exe")
 
